7c787178cdd7d8f93fd3278581bcc3c22def428e
gwangho <145312516+Wigwangho@users.noreply.github.com>
2023-10-11 21:29:27 +0900

    Create SoundOFBugs.java
    각 클래스들로부터 Sound 추상메소드를 받아 실행하는 main 클래스

---
+++ interface_basic/SoundOFBugs.java
@@ -0,0 +1,16 @@
+package com.inter;
+
+public class SoundOFBugs {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ButterFly bf = new ButterFly();
+		Mosquito mq = new Mosquito();
+		Grasshopper gh = new Grasshopper();
+		bf.Sound();
+		mq.Sound();
+		gh.Sound();
+		
+	}
+
+}
