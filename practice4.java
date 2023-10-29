a23f65daeb30684b4060d90c9689ef85f8bdd2bc
gwangho <145312516+Wigwangho@users.noreply.github.com>
2023-10-29 23:36:57 +0900

    Create practice4.java
    ㅋ

---
+++ practice4.java
@@ -0,0 +1,20 @@
+package com.week41;
+//chat gpt도 틀린 문제
+public class practice4 {
+	public static void main(String[] args) {
+		int x= 10;
+		 do {
+		 x--;
+		 
+		 System.out.println("했어");
+		 } while(x< 10);//첫번째 실행: x는 10이므로 do 문 내에서 정상적으로 실행된다. 그 후 x에서 1을 차감한다.
+    // 두 번째 실행: x는 9이므로 while문의 조건을 충족하지 않아 do-while 문을 빠져나오게 된다.
+	}
+	 
+	/*How many times will line 37 be executed?  
+	A88. ten times
+	B. zero times
+	C. one to me times  0
+	D. more than ten times  */
+
+}
